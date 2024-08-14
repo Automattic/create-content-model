980034620f91b34514d5bca0cf306c0836997cca
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-08-14 16:55:10 -0300

    Expose group variations

--- includes/runtime/expose-cpt-fields.php
+++ includes/runtime/expose-cpt-fields.php
@@ -14,6 +14,8 @@ add_filter( 'get_block_type_variations', 'register_block_variations', 10, 2 );
  * @param WP_Block_Type $block_type The block type.
  */
 function register_block_variations( $variations, $block_type ) {
+	// TODO: Do not expose block variations when creating a content model.
+
 	$block_variations = get_block_variations_from_content_models();
 
 	if ( empty( $block_variations[ $block_type->name ] ) ) {
@@ -21,15 +23,29 @@ function register_block_variations( $variations, $block_type ) {
 	}
 
 	foreach ( $block_variations[ $block_type->name ] as $block_variation ) {
-		$variations[] = array(
+		$variation = array(
+			'name'       => '__' . $block_variation['block_variation_name'] . '/' . $block_type->name,
 			'title'      => $block_variation['block_variation_name'],
 			'category'   => $block_variation['cpt_slug'] . '-fields',
-			'attributes' => array(
-				'metadata' => array(
-					'bindings' => $block_variation['bindings'],
-				),
-			),
+			'attributes' => array(),
 		);
+
+		if ( 'core/group' === $block_type->name ) {
+			$variation['innerBlocks'] = array(
+				array(
+					'core/paragraph',
+					array(
+						'content' => $block_variation['bindings']['content']['args']['key'],
+					),
+				),
+			);
+		}
+
+		$variation['attributes']['metadata'] = array(
+			'bindings' => $block_variation['bindings'],
+		);
+
+		$variations[] = $variation;
 	}
 
 	return $variations;
@@ -108,4 +124,41 @@ function register_field_categories( $categories ) {
 	}
 
 	return $categories;
+}
+
+add_filter( 'pre_render_block', 'render_group_variation', 99, 2 );
+
+/**
+ * Since there are no official bindings for group variations and its inner blocks,
+ * we need to inject the inner blocks and HTML ourselves.
+ *
+ * @param string|null $pre_render The pre-rendered content.
+ * @param array       $parsed_block The parsed block.
+ *
+ * @return string|null The rendered block.
+ */
+function render_group_variation( $pre_render, $parsed_block ) {
+	$bound_meta_field = $parsed_block['attrs']['metadata']['bindings']['content']['args']['key'] ?? null;
+
+	if ( 'core/group' !== $parsed_block['blockName'] || ! $bound_meta_field ) {
+		return $pre_render;
+	}
+
+	$content = get_post_meta( get_the_ID(), $bound_meta_field, true );
+
+	if ( ! $content ) {
+		return $pre_render;
+	}
+
+	$parsed_block['innerBlocks']  = parse_blocks( $content );
+	$parsed_block['innerHTML']    = inject_content_into_block_markup( $content, $parsed_block['innerHTML'] );
+	$parsed_block['innerContent'] = array( $parsed_block['innerHTML'] );
+
+	remove_filter( 'pre_render_block', 'render_group_variation', 99 );
+
+	$rendered_group = render_block( $parsed_block );
+
+	add_filter( 'pre_render_block', 'render_group_variation', 99, 2 );
+
+	return $rendered_group;
 }
