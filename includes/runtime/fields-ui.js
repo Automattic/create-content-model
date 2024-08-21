33a52484c343c0de37a41ba6ad683705020f4cfb
Brian Coords <bacoords@gmail.com>
2024-08-21 16:11:33 -0700

    fix for additional blocks added to the post content group

--- includes/runtime/fields-ui.js
+++ includes/runtime/fields-ui.js
@@ -79,22 +79,20 @@ const SUPPORTED_BLOCKS = [
 	'core/button',
 ];
 
-const parseBlocks = ( blocks, setEditMode ) => {
+const parseBlocks = ( blocks, setEditMode, forceEnabled = false ) => {
 	blocks.forEach( ( block ) => {
 		if (
 			block.innerBlocks.length > 0 &&
 			block.attributes.metadata?.bindings
 		) {
-			// dispatch( 'core/block-editor' ).updateBlock( block.clientId, {
-			// 	...block,
-			// 	attributes: {
-			// 		...block.attributes,
-			// 		templateLock: 'contentOnly',
-			// 	},
-			// } );
+			// This is a group block with bindings, probably content.
 			setEditMode( block.clientId, '' );
+
+			if ( block.innerBlocks ) {
+				parseBlocks( block.innerBlocks, setEditMode, true );
+			}
 		} else if ( block.innerBlocks.length > 0 ) {
-			// setEditMode( block.clientId, '' );
+			// This is a group block with no bindings, probably layout.
 			dispatch( 'core/block-editor' ).updateBlock( block.clientId, {
 				...block,
 				attributes: {
@@ -103,17 +101,18 @@ const parseBlocks = ( blocks, setEditMode ) => {
 				},
 			} );
 			setEditMode( block.clientId, 'disabled' );
+
+			if ( block.innerBlocks ) {
+				parseBlocks( block.innerBlocks, setEditMode );
+			}
 		} else if (
-			SUPPORTED_BLOCKS.includes( block.name ) &&
-			block.attributes.metadata?.bindings
+			( SUPPORTED_BLOCKS.includes( block.name ) &&
+				block.attributes.metadata?.bindings ) ||
+			forceEnabled
 		) {
 			setEditMode( block.clientId, '' );
 		} else {
 			setEditMode( block.clientId, 'disabled' );
-		}
-
-		if ( block.innerBlocks ) {
-			parseBlocks( block.innerBlocks, setEditMode );
 		}
 	} );
 };
