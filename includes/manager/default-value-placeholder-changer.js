25766388a2e3a9413bc1e399ba16bcb168aca184
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-08-29 10:51:50 -0300

    Change default placeholder when defining the data model

---
+++ includes/manager/default-value-placeholder-changer.js
@@ -0,0 +1,47 @@
+import { __, sprintf } from '@wordpress/i18n';
+import { useEffect } from '@wordpress/element';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { registerPlugin } from '@wordpress/plugins';
+
+const ContentModelDefaultValuePlaceholderChanger = () => {
+	const boundBlocks = useSelect( ( select ) => {
+		const blocks = select( blockEditorStore ).getBlocks();
+		const map = {};
+
+		const processBlock = ( block ) => {
+			const name = block.attributes?.metadata?.name;
+
+			if ( name ) {
+				map[ block.clientId ] = block.attributes.metadata.name;
+			}
+
+			// Process inner blocks if they exist, like core/button is inside core/buttons.
+			if ( block.innerBlocks && block.innerBlocks.length > 0 ) {
+				block.innerBlocks.forEach( processBlock );
+			}
+		};
+
+		blocks.forEach( processBlock );
+
+		return map;
+	}, [] );
+
+	const { updateBlockAttributes } = useDispatch( blockEditorStore );
+
+	useEffect( () => {
+		Object.entries( boundBlocks ).forEach( ( [ blockId, blockName ] ) => {
+			updateBlockAttributes( blockId, {
+				placeholder: sprintf(
+					// translators: %s is the block name.
+					__( 'Enter the default value for %s' ),
+					blockName
+				),
+			} );
+		} );
+	}, [ boundBlocks, updateBlockAttributes ] );
+};
+
+registerPlugin( 'content-model-default-value-placeholder-changer', {
+	render: ContentModelDefaultValuePlaceholderChanger,
+} );
