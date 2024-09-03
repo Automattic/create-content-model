05e097809375f6abfeea84bacf1d98beb3865e65
Brian Coords <bacoords@gmail.com>
2024-09-03 06:39:03 -0700

    fixes typos (#101)

--- includes/manager/src/register-cpt-settings-panel.js
+++ includes/manager/src/register-cpt-settings-panel.js
@@ -2,7 +2,7 @@ import { registerPlugin } from '@wordpress/plugins';
 import { CPTSettingsPanel } from './components/cpt-settings-panel';
 
 export const registerCPTSettingsPanel = () => {
-	registerPlugin( 'create-content-model-cpt-settings-pannel', {
+	registerPlugin( 'create-content-model-cpt-settings-panel', {
 		render: CPTSettingsPanel,
 	} );
 };
