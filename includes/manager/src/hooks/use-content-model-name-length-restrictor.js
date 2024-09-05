6ce3addf80d70d525926e3578eea427ed7600ae5
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-09-04 21:49:35 -0300

    Prevent saving content model without a name

--- includes/manager/src/hooks/use-content-model-name-length-restrictor.js
+++ includes/manager/src/hooks/use-content-model-name-length-restrictor.js
@@ -5,7 +5,8 @@ import { store as editorStore } from '@wordpress/editor';
 import { store as noticesStore } from '@wordpress/notices';
 
 export const useContentModelNameLengthRestrictor = () => {
-	const { editPost } = useDispatch( editorStore );
+	const { editPost, lockPostSaving, unlockPostSaving } =
+		useDispatch( editorStore );
 	const { createNotice } = useDispatch( noticesStore );
 
 	const title = useSelect( ( select ) =>
@@ -13,8 +14,18 @@ export const useContentModelNameLengthRestrictor = () => {
 	);
 
 	useEffect( () => {
-		if ( title && title.length > 20 ) {
-			editPost( { title: title.substring( 0, 20 ) } );
+		const trimmedTitle = title?.trim() ?? '';
+
+		if ( trimmedTitle.length === 0 ) {
+			lockPostSaving( 'title-empty-lock' );
+			return;
+		}
+
+		unlockPostSaving( 'title-empty-lock' );
+
+		editPost( { title: trimmedTitle.substring( 0, 20 ) } );
+
+		if ( trimmedTitle.length > 20 ) {
 			createNotice(
 				'warning',
 				__( 'Title is limited to 20 characters.' ),
@@ -24,5 +35,5 @@ export const useContentModelNameLengthRestrictor = () => {
 				}
 			);
 		}
-	}, [ title, editPost, createNotice ] );
+	}, [ title, lockPostSaving, unlockPostSaving, editPost, createNotice ] );
 };
